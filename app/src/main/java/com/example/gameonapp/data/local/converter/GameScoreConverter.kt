b91da48dd038524a4f16f700f83b05bccb347854
Ianis_Teja <ianist2001@gmail.com>
2025-12-18 17:03:09 +0200

    Created a game converter

---
+++ app/src/main/java/com/example/gameonapp/data/local/converter/GameScoreConverter.kt
@@ -0,0 +1,40 @@
+package com.example.gameonapp.data.local.converter
+
+import androidx.room.TypeConverter
+import com.example.gameonapp.data.local.model.GameScore
+import com.example.gameonapp.data.local.model.SimpleScore
+import com.example.gameonapp.data.local.model.VolleyballScore
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
+
+
+class GameScoreConverter {
+
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromGameScore(score: GameScore?): String {
+        if (score == null) return ""
+        val json = gson.toJsonTree(score).asJsonObject
+        // Add a type field
+        when (score) {
+            is SimpleScore -> json.addProperty("type", "simple")
+            is VolleyballScore -> json.addProperty("type", "volleyball")
+        }
+        return gson.toJson(json)
+    }
+
+    @TypeConverter
+    fun toGameScore(jsonString: String?): GameScore? {
+        if (jsonString.isNullOrBlank()) return null
+        val jsonElement = JsonParser.parseString(jsonString).asJsonObject
+        val type = jsonElement.get("type")?.asString ?: return null
+
+        return when (type) {
+            "simple" -> gson.fromJson(jsonString, SimpleScore::class.java)
+            "volleyball" -> gson.fromJson(jsonString, VolleyballScore::class.java)
+            else -> null
+        }
+    }
+}
