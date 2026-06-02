f5dd19379008e6ec67b0bcf852010a2be8e945c5
Ianis_Teja <ianist2001@gmail.com>
2026-06-02 17:27:55 +0300

    Viewmodel improvements, game details improvements

--- app/src/main/java/com/example/gameonapp/presentation/components/ScoreColumn.kt
+++ app/src/main/java/com/example/gameonapp/presentation/components/ScoreColumn.kt
@@ -1,5 +1,6 @@
 package com.example.gameonapp.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -19,8 +20,11 @@ import androidx.wear.compose.material3.Icon
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.Text
 import com.example.gameonapp.data.local.model.GameScore
+import com.example.gameonapp.data.local.model.PadelMatchState
 import com.example.gameonapp.data.local.model.SimpleScore
+import com.example.gameonapp.data.local.model.TennisMatchState
 import com.example.gameonapp.data.local.model.VolleyballScore
+import com.example.gameonapp.presentation.theme.ColorScheme
 import com.example.gameonapp.utils.GameType
 import com.example.gameonapp.utils.toPascalCase
 
@@ -58,6 +62,46 @@ fun ScoreColumn(modifier: Modifier = Modifier, gameType: GameType, gameScore: Ga
             )
 
             is VolleyballScore -> {
+                Text(
+                    text = gameScore.getFinalScore(),
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = gameScore.getSetScores(),
+                    style = MaterialTheme.typography.titleSmall.copy(
+                        fontWeight = FontWeight.SemiBold
+                    )
+                )
+            }
+
+            is TennisMatchState -> {
+                Text(
+                    text = gameScore.getFinalScore(),
+                    style = MaterialTheme.typography.titleMedium.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth(0.6f)
+                        .height(2.dp)
+                        .background(color = ColorScheme.primary)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = gameScore.getSetScores(),
+                    style = MaterialTheme.typography.titleSmall.copy(
+                        fontWeight = FontWeight.SemiBold,
+                        lineHeight = 20.sp
+                    )
+                )
+            }
+
+            is PadelMatchState -> {
                 Text(
                     text = gameScore.getFinalScore(),
                     style = MaterialTheme.typography.titleMedium.copy(
